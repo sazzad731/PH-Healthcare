5cea76bf4847887b9e8ecdaa774c84c9cd40773f
sazzad731 <sajjadho731@gmail.com>
2026-06-30 22:51:37 +0600

    [DONE]: configure env variables

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import app from "./app";
+import { envConfig } from "./config/env";
 
 
 const bootstrap = () => { 
   try {
-    app.listen(process.env.PORT || 5000, () => { 
-      console.log(`Server is running on http://localhost:${process.env.PORT || 5000}`);
+    app.listen(envConfig.PORT, () => { 
+      console.log(`Server is running on http://localhost:${envConfig.PORT}`);
     })
   } catch (error) {
     console.error('Failed to start server: ', error)
