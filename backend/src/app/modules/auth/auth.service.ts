11606d0ba9d1c5b171efd03bc7d5c6ad8ff83bbd
sazzad731 <sajjadho731@gmail.com>
2026-07-01 00:02:15 +0600

    [DONE]: add rollback in patient transaction

--- backend/src/app/modules/auth/auth.service.ts
+++ backend/src/app/modules/auth/auth.service.ts
@@ -27,17 +27,27 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
   }
 
   // Create Patient Profile in Transaction After Sign up of Patient In User Model
-  const patient = await prisma.$transaction(async (tx) => { 
-    return await tx.patient.create({
-      data: {
-        userId: data.user.id,
-        name: payload.name,
-        email: payload.email,
+  try {
+    const patient = await prisma.$transaction(async (tx) => {
+      return await tx.patient.create({
+        data: {
+          userId: data.user.id,
+          name: payload.name,
+          email: payload.email,
+        },
+      });
+    });
+
+    return { ...data, patient };
+  } catch (error) {
+    console.log("Transaction error: ", error)
+    await prisma.user.delete({
+      where: {
+        id: data.user.id
       }
     })
-  })
-  
-  return {...data, patient}
+    throw error;
+  }
 };
 
 
